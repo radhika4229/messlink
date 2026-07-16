fdccf750ae85ccef4dacee3b0e2d7470ad1489c5
Priya Kumari <chaudhary21priya@gmail.com>
2026-07-16 14:55:48 +0530

    wip: mesh status fetch and toggle wiring

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,11 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-// import TestHarness from './TestHarness.jsx'
+import TestHarness from './TestHarness.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
-    {/* <TestHarness /> */}
+    {/* <App /> */}
+    <TestHarness />
   </StrictMode>,
 )
